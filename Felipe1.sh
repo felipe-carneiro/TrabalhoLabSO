42a275bd24b4ead55f637dedad588549d7a50158
felipe-carneiro <felipefxoc@gmail.com>
2018-06-20 19:51:29 -0300

    Trabalho lab SO

--- Felipe1.sh
+++ Felipe1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 $1
-git config --global user.name "Felipe Carneiro"
+git config --global user.name "felipe-carneiro"
 git config --global user.email "felipefxoc@gmail.com"
 git add .
 git commit -m "$1"
